56823a31bc350c9b1c5d9fb1c362238d48bf63e3
Ann Dang <dangitsann@gmail.com>
2022-11-04 00:20:37 -0400

    tweaking

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,20 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+import styles from './index.module.css'
+
 export default function Home() {
   return (
     <div className="container">
       <Head>
         <title>Peddler's Paradise</title>
         <link rel="icon" href="/favicon.ico" />
-        <center><img src="/whitelogo.png" alt="Logo" className="logo"/></center>
+        <center><img src="/whitelogo.png" alt="Logo" className={styles.logo}/></center>
       </Head>
 
       <main>
         <iframe src="/peddlers menu.pdf" width="200%" height="800"></iframe>
       </main>
-      <Footer />
     </div>
   )
 }
